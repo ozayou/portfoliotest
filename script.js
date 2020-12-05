2daec5ff90b6746ef91f063bb782530da471ee90
ozayou <75507551+ozayou@users.noreply.github.com>
2020-12-06 08:26:14 +0900

    initial commit
    練習です。

---
+++ script.js
@@ -0,0 +1,14 @@
+$(function () {
+    $('.hamburger').click(function () {
+        $(this).toggleClass('active');
+
+        if ($(this).hasClass('active')) {
+            $('.globalMenuSp').addClass('active');
+        } else {
+            $('.globalMenuSp').removeClass('active');
+        }
+    });
+    $('#menu a[href]').on('click', function (event) {
+        $('.hamburger').trigger('click');
+    });
+});

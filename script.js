beed791fd90227f3eae4cad093cd564ebd5763a3
ozayou <75507551+ozayou@users.noreply.github.com>
2021-01-22 08:10:39 +0900

    Update script.js

--- script.js
+++ script.js
@@ -11,4 +11,25 @@ $(function () {
     $('#menu a[href]').on('click', function (event) {
         $('.hamburger').trigger('click');
     });
+
+    $(window).scroll(function () {
+        $('.fadein').each(function () {
+            var elemPos = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > elemPos - windowHeight + 200) {
+                $(this).addClass('scrollin');
+            }
+        });
+    });
+
+    // $(window).on("scroll", function () {
+    //     if ($(window).scrollTop() > 50) {
+    //         $(".left-header a, .hamburger span").addClass("activecolor");
+    //     } else {
+    //         //remove the background property so it comes transparent again (defined in your css)
+    //         $(".left-header a, .hamburger span").removeClass("activecolor");
+    //     }
+    // });
+
 });
